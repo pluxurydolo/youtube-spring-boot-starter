5c4d3549d8811a11c93df5a05e4e1e642905fc92
Гвоздев Александр <aleksandr.gvozdev@gazprombank.ru>
2026-04-13 13:57:10 +0300

    13.04.2026

--- src/main/java/com/pluxurydolo/youtube/config/YouTubeAutoConfiguration.java
+++ src/main/java/com/pluxurydolo/youtube/config/YouTubeAutoConfiguration.java
@@ -1,7 +1,6 @@
 package com.pluxurydolo.youtube.config;
 
 import com.pluxurydolo.youtube.properties.YouTubeProperties;
-import com.pluxurydolo.youtube.properties.YouTubeRateLimitProperties;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -9,10 +8,7 @@ import org.springframework.context.annotation.Import;
 
 @AutoConfiguration
 @ConditionalOnProperty(prefix = "youtube", name = "enabled", havingValue = "true")
-@EnableConfigurationProperties({
-    YouTubeProperties.class,
-    YouTubeRateLimitProperties.class
-})
+@EnableConfigurationProperties(YouTubeProperties.class)
 @Import({
     YouTubeCoreConfiguration.class,
     YouTubeOAuthConfiguration.class,
--- src/main/java/com/pluxurydolo/youtube/properties/YouTubeRateLimitProperties.java
+++
@@ -1,7 +0,0 @@
-package com.pluxurydolo.youtube.properties;
-
-import org.springframework.boot.context.properties.ConfigurationProperties;
-
-@ConfigurationProperties(prefix = "youtube.rate-limit")
-public record YouTubeRateLimitProperties(int threshold) {
-}
