31a854abf81415596bf56ea0c10d1e177778b902
Гвоздев Александр <aleksandr.gvozdev@gazprombank.ru>
2026-04-17 22:06:56 +0300

    17.04.2026

--- src/main/java/com/pluxurydolo/youtube/config/YouTubeAutoConfiguration.java
+++ src/main/java/com/pluxurydolo/youtube/config/YouTubeAutoConfiguration.java
@@ -2,12 +2,10 @@ package com.pluxurydolo.youtube.config;
 
 import com.pluxurydolo.youtube.properties.YouTubeProperties;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Import;
 
 @AutoConfiguration
-@ConditionalOnProperty(prefix = "youtube", name = "enabled", havingValue = "true")
 @EnableConfigurationProperties(YouTubeProperties.class)
 @Import({
     YouTubeCoreConfiguration.class,
